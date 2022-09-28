6a4f61fb5b01a9253ebf5d163da5d50b41d6bf6e
Llama <dog@dog.dog>
2022-09-28 15:37:29 -0400

    source update

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ git add build/
 git commit -m "source update"
 git push main main
 git checkout site
-git checkout main build/ index.html sheet/
+git checkout main build/
+git checkout main index.html
+git checkout main sheet/
 git commit -m "build"
 git push main site
 git checkout main
